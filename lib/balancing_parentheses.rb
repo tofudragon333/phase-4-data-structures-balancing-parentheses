59561daaba2b76a56fccc44e100304834ef2cb9e
Ashley Wong <tofudragon333@gmail.com>
2022-09-23 12:53:44 -0400

    commit tree e8aa43c86d0514c1162656224771c83a1df1319d

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,15 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(string)
+    stack = Stack.new
+
+    string.chars.each do |c|
+    if c == ")" && stack.peek == "("
+        stack.pop
+    else
+         stack.push(c)
+    end
+    end
+    stack.size
+end
